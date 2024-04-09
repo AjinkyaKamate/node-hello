ba2a2e98ffa23af71ec4abbe48c644fdecc7af6a
AjinkyaKamate <150146008+AjinkyaKamate@users.noreply.github.com>
2024-04-09 19:51:11 +0530

    Update index.js
    Adding login page

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+// Import required module
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello World!!!\n'
-  res.end(msg);
+// Initialize the Express application
+const app = express();
+
+// Configure Express
+app.use(express.urlencoded({ extended: true }));
+
+// Define routes
+app.get('/login', function(req, res) {
+  res.send('<form action="/login" method="post"><div><label>Username:</label><input type="text" name="username"/></div><div><label>Password:</label><input type="password" name="password"/></div><div><input type="submit" value="Log In"/></div></form>');
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+app.post('/login', function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Here you would typically look up the user in your database
+  // For this example, we'll just check if the username and password are 'admin'
+  if (username === 'admin' && password === 'admin') {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/', function(req, res) {
+  res.send('Welcome, admin');
+});
+
+// Start the server
+app.listen(3000, function() {
+  console.log('App listening on port 3000');
 });
